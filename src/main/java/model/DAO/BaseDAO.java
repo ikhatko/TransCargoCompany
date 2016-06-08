7894d8f0b51218fe0e8c713ee06f990e6f7ef2b0
kislota <hatko.id@gmail.com>
2016-06-08 18:00:03 +0300

    BaseDAO added

---
+++ src/main/java/model/DAO/BaseDAO.java
@@ -0,0 +1,48 @@
+package model.DAO;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public abstract class BaseDAO<T> {
+    private final Class<T> entityType;
+    private final SessionFactory sessionFactory;
+
+    protected BaseDAO(Class<T> entityType, SessionFactory sessionFactory) {
+        this.entityType = entityType;
+        this.sessionFactory = sessionFactory;
+    }
+
+    public T getById(int id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        T getEntity = (T) session.get(entityType, id);
+        transaction.commit();
+        session.close();
+        return getEntity;
+    }
+
+    public void save(T entity) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(entity);
+        transaction.commit();
+        session.close();
+    }
+
+    public void update(T entity) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(entity);
+        transaction.commit();
+        session.close();
+    }
+
+    public void deleteById(int id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(getById(id));
+        transaction.commit();
+        session.close();
+    }
+}
