a93505943f3b2179e77edf7d49759f3e51851bcb
kislota <hatko.id@gmail.com>
2016-06-08 16:08:35 +0300

    added error handler

---
+++ src/main/java/controller/Servlet/ErrorHandler/ErrorHandler.java
@@ -0,0 +1,58 @@
+package controller.Servlet.ErrorHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ErrorHandler")
+public class ErrorHandler extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        catchError(req, resp);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        catchError(req, resp);
+    }
+
+    private void catchError(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Throwable throwable = (Throwable) req.getAttribute("javax.servlet.error.exception");
+        Integer statusCode = (Integer) req.getAttribute("javax.servlet.error.status_code");
+        String requestUri = (String) req.getAttribute("javax.servlet.error.request_uri");
+        String servletName = (String) req.getAttribute("javax.servlet.error.servlet_name");
+
+        if (servletName == null) {
+            servletName = "Unknown";
+        }
+        if (requestUri == null) {
+            requestUri = "Unknown";
+        }
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.write("<html><head><title>Exception/Error Details</title></head><body>");
+        if (statusCode != 500) {
+            out.write("<h3>Error Details</h3>");
+            out.write("<strong>Status Code</strong>:" + statusCode + "<br>");
+            out.write("<strong>Requested URI</strong>:" + requestUri);
+        } else {
+            out.write("<h3>Exception Details</h3>");
+            out.write("<strong>Status Code</strong>:" + statusCode + "<br>");
+            out.write("<ul><li>Servlet Name:" + servletName + "</li>");
+            out.write("<li>Exception Name:" + throwable.getClass().getName() + "</li>");
+            out.write("<li>Requested URI:" + requestUri + "</li>");
+            out.write("<li>Exception Message:" + throwable.getMessage() + "</li>");
+            out.write("</ul>");
+        }
+
+        out.write("<br><br>");
+        out.write("<a href=\"login.html\">Login Page</a>");
+        out.write("</body></html>");
+    }
+}
